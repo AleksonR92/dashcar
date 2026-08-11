e691294e789383f21bdff130a5e217ceceec27cc
AleksonR92 <133175459+AleksonR92@users.noreply.github.com>
2026-08-11 06:52:51 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Estratégia: tenta buscar na rede primeiro (pra sempre pegar a versão mais nova quando online);
 // se não conseguir (sem internet), usa a cópia salva em cache — garantindo que o app abra offline.
 
-const CACHE_NAME = 'controle-veiculo-v21'; // ao publicar uma nova revisão, aumente esse número (v22, v23...)
+const CACHE_NAME = 'controle-veiculo-v22'; // ao publicar uma nova revisão, aumente esse número (v23, v24...)
 const ASSETS = [
   './',
   './index.html',
